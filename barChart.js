a150434adfd2a061748b1d699bfa0cb01d313da7
dcarino <niel.carino@gmail.com>
2016-03-08 15:15:06 +0100

    Vertical bars instead of horizontal
    Text is messed up though

--- barChart.js
+++ barChart.js
@@ -1,42 +1,46 @@
-var width = 420, //Width of chart
-	barHeight = 20; //Height (or Thickness) of bar
+var height = 500, //Height of chart
+	barWidth = 70; //Height (or Thickness) of bar
 
-var x = d3.scale.linear()
-	.range([0, width]);
+var y = d3.scale.linear()
+	.range([0, height]);
 
 var chart = d3.select(".chart") 
-	.attr("width", width)
+	.attr("height", height)
 
 d3.csv("data/trainees.csv", type, function(error, data) {
-	x.domain([0, d3.max(data, function(d){
+	y.domain([0, d3.max(data, function(d){
 		return d.Points;}
 	)]);
 
-	chart.attr("height", barHeight * data.length); //Set the height of the chart to sum of the barHeights
+	chart.attr("width", barWidth * data.length); //Set the height of the chart to sum of the barHeights
 
 	var bar = chart.selectAll("g") //g is a group of SVG elements
 		.data(data) //join data to the selection
 	    .enter().append("g") //Initiates missing elements
 	    .attr("transform", function(d, i) { 
-	    	return "translate(0," + i * barHeight + ")"; });
+	    	return "translate("+ i * barWidth + ",0)"; });
 
 	bar.append("rect")
-	    .attr("width", function(d) { return x(d.Points); })
-	    .attr("height", barHeight - 1);
+	    .attr("height", function(d) { 
+	    	return y(d.Points); })
+	    .attr("width", barWidth - 2);
 
 	bar.append("text")
-	    .attr("x", function(d) { return x(d.Points) - 3; })
-	    .attr("y", barHeight / 2)
-	    .attr("dy", ".35em")
-	    .text(function(d) { return d.Points; });
+	    .attr("y", function(d) { return y(d.Points) - 3; })
+	    .attr("x", barWidth / 2)
+	    .attr("dx", ".35em")
+	    .text(function(d) { return d.Points; })
+	    .attr("transform", function(){
+	    	return "rotate(-10)";
+	    });
 
 	bar.append("text")
-	    .attr("x", 70)
-	    .attr("y", barHeight / 2)
-	    .attr("dy", ".35em")
+	    .attr("y", 5)
+	    .attr("x", barWidth / 2)
+	    .attr("dx", ".35em")
 	    .text(function(d) { 
 	    	return d.Name; })
-	    .attr("text-anchor", "start");
+	    
 });
 
 function type(d) {
