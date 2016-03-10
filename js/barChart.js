f1142a2da4442f252495e9f9ffd6af36bc6b257d
dcarino <niel.carino@gmail.com>
2016-03-10 13:05:33 +0100

    Added all trainees
    Can't fit them all

--- js/barChart.js
+++ js/barChart.js
@@ -1,15 +1,16 @@
 var width = parseInt(d3.select("body").style("width")), //Width of chart
-	barHeight = parseInt(d3.select("body").style("height"))/7; //Height (or Thickness) of bar
+//var width = 1000,
+	barHeight = parseInt(d3.select("body").style("height"))/10; //Height (or Thickness) of bar
 
 var x = d3.scale.linear()
-	.range([0, width]);
+	.range([0, 210]);
 
 var chart = d3.select(".chart") 
 	.attr("width", width)
 
 d3.csv("data/trainees.csv", type, function(error, data) {
 	x.domain([0, d3.max(data, function(d){
-		return d.Points;}
+		return d.Points_Ep5;}
 	)]);
 
 	chart.attr("height", barHeight * data.length); //Set the height of the chart to sum of the barHeights
@@ -21,14 +22,14 @@ d3.csv("data/trainees.csv", type, function(error, data) {
 	    	return "translate(0," + i * barHeight + ")"; });
 
 	bar.append("rect")
-	    .attr("width", function(d) { return x(d.Points); })
+	    .attr("width", function(d) { return x(d.Points_Ep5); })
 	    .attr("height", barHeight - 1);
 
 	bar.append("text")
-	    .attr("x", function(d) { return x(d.Points) - 3; })
+	    .attr("x", function(d) { return x(d.Points_Ep5) - 3; })
 	    .attr("y", barHeight / 2)
 	    .attr("dy", ".35em")
-	    .text(function(d) { return d.Points; });
+	    .text(function(d) { return d.Points_Ep5; });
 
 	bar.append("text")
 		.attr("id","barName")
@@ -41,6 +42,6 @@ d3.csv("data/trainees.csv", type, function(error, data) {
 });
 
 function type(d) {
-	d.value = +d.Points; // coerce to number
+	d.value = +d.Points_Ep5; // coerce to number
 	return d;
 }
